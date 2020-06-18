3abb9cd6ca1461c6e4063253ed559c907a310c31
Aideen Fay <me@aideen.dev>
2020-06-18 22:42:47 +0100

    change date to float and data tweaks

--- src/models/book.go
+++ src/models/book.go
@@ -4,7 +4,7 @@ type Book struct {
 	ID          int      `json:"id"`
 	Title       string   `json:"title"`
 	Author      []string `json:"author"`
-	DateRead    string   `json:"date_read"`
+	DateRead    int      `json:"date_read"`
 	Description string   `json:"description"`
 	Photo       string   `json:"photo"`
 	ISBN        string   `json:"isbn"`
